5a605137327438c5a75ec6177f0cb8adff9c8321
ibronit <berbadger@gmail.com>
2018-06-23 10:15:32 +0200

    wildcard get api + code formatting

--- module/UrlStorage.js
+++ module/UrlStorage.js
@@ -3,7 +3,7 @@ const ShorthandIsNotUnique = require('../exception/ShorthandIsNotUnique');
 
 let storage = [];
 
-exports.store = function (url, shorthand = null) {    
+exports.store = function (url, shorthand = null) {
     if (shorthand) {
         validateShorthand(shorthand);
         const urls = { original_url: url, shorthand: shorthand };
@@ -11,22 +11,22 @@ exports.store = function (url, shorthand = null) {
         return urls;
     }
 
-    shorthand = {shorthand: UrlShortener.generateUrl()};
+    shorthand = { shorthand: UrlShortener.generateUrl() };
     storage.push({ original_url: url, ...shorthand });
     return shorthand;
 }
 
-function getByShorthand (shorthand) {
+function getByShorthand(shorthand) {
     return storage.find((obj) => {
         return obj.shorthand === shorthand;
     });
-} 
+}
 
-function validateShorthand (shorthand) {
+function validateShorthand(shorthand) {
     if (getByShorthand(shorthand)) {
         const err = new ShorthandIsNotUnique(`Shorthand: "${shorthand}" already exists`);
         throw err;
-    }    
+    }
 }
 
 exports.storage = storage;
